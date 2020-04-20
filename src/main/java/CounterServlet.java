1495a88529cf135caa0953331dc8d4e7e17157bd
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-04-20 16:02:59 -0500

    Servlets exercise completed, working on final counter bonus

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,40 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// TODO: 5. Create a page view counter
+//          a. Create a page that displays a number that goes up by one every time the /count page is viewed.
+//          ------------BONUS------------
+//          b. Allow the user to pass a parameter in the query string to reset the counter.
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+
+    // --------- INITIALIZE ---------
+    private int counter = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+        // --------- RESPONSE TYPE ---------
+        response.setContentType("text/html"); // set the response type
+
+        // --------- RESPONSE ACTION ---------
+        String reset = request.getParameter("reset"); // Allow user to reset the counter variable set
+
+        PrintWriter out = response.getWriter(); // store the response.getWriter() in the variable out
+
+        if (reset.equals("reset"))
+            counter = 0; // reset counter to zero
+
+        counter++; // increment the counter for every request
+
+        out.println("<h1>The current page count is: " + counter+ "</h1>");
+    }
+
+}
+
--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,47 @@
+// TODO: 1. Fork and clone the Adlister Repository
+
+// TODO: 2. Checkout the servlets tag and branch off of it
+//          a. git checkout servlets
+//          b. git branch servlets-exercise
+//          c. git checkout servlets-exercise
+
+// TODO: 3. Configure IntelliJ for the project
+//          a. See this section in the appendix for more details.
+
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+
+// TODO: 4. Finish up the Hello World Servlet
+//          a. Create a page that displays "Hello World!" when a user visits /hello
+//          ------------BONUS------------
+//          b. Make the page say "Hello, <name>!" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
+
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        // --------- RESPONSE TYPE ---------
+        response.setContentType("text/html"); // set the response type
+
+        // --------- RESPONSE ACTION ---------
+        String name = request.getParameter("name");
+
+        PrintWriter out = response.getWriter(); // store the response.getWriter() in the variable out
+
+        if (name != null)
+            out.println("<h1>Hello, " + name + "!</h1>"); // use the out variable --> print to the html the Greeting --> "<h1>Hello, World!</h1>"
+        else
+            out.println("<h1>Hello, World!</h1>");
 
     }
+
 }
